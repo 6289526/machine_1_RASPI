69d5e2e2b0aa24eedc3b9a147c08aedb17030120
tokudairobocon <tokudairobocon@gmail.com>
2021-03-24 17:36:07 +0900

    余分なコメントアウトを削除

--- marker_detect_1.cpp
+++ marker_detect_1.cpp
@@ -1,6 +1,5 @@
 ﻿// marker_detect_1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
-//#include "pch.h"
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
 #include <iostream>
@@ -39,7 +38,6 @@ struct Send_Data{
 };
 Send_Data send_data = {};
 
-//serial Serial("/dev/ttyACM0", B9600);
 Safe_Server server_tcp;
 
 int main(int argc, const char* argv[])
@@ -72,9 +70,7 @@ int main(int argc, const char* argv[])
 	thread th_server([&] {server_tcp.run(); });
 
 	VideoCapture cap(0);  //カメラの映像の読み込み
-    //cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
     cap.set(3, 1280);
-	//cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 	cap.set(4, 720);
 
 	Mat mirror_image;  //表示画面の左右反転の用意
@@ -108,7 +104,6 @@ int main(int argc, const char* argv[])
 			this_thread::sleep_for(chrono::seconds(1));
 			continue;
 		}
-			//return -1;
 
 		cap >> image;  //imageにカメラの映像を代入
 		if (image.empty())  //映像がないときはとばす 
@@ -144,12 +139,7 @@ int main(int argc, const char* argv[])
 			}
 			marker_center[i][0] = marker_center[i][0] / 4;  //x座標
 			marker_center[i][1] = marker_center[i][1] / 4;  //y座標
-			//average_center[0] = average_center[0] + marker_center[i][0];
-			//average_center[1] = average_center[1] + marker_center[i][1];
 		}
-		//average_center[0] = average_center[0] / marker_ids.size();
-		//average_center[1] = average_center[1] / marker_ids.size();
-
 
 		//4つのマーカーのスクリーン座標系の中心の取得
 		int z = 0;
@@ -199,18 +189,6 @@ int main(int argc, const char* argv[])
 		//マーカーの3軸の検出、ｘｙｚの取得
 		if (marker_ids.size() > 0)
 		{
-			/*
-			double angleX[6] = { 0,0,0,0,0,0 };
-			double angleY[6] = { 0,0,0,0,0,0 };
-			double angleZ[6] = { 0,0,0,0,0,0 };
-			double ave_angleX = 0;
-			double ave_angleY = 0;
-			double ave_angleZ = 0;
-
-			double distanceX = 0;
-			double distanceY = 0;
-			double distanceZ = 0;
-			double distanceR = 0;*/
 			ave_angleX = 0;
 			ave_angleY = 0;
 			ave_angleZ = 0;
@@ -245,19 +223,13 @@ int main(int argc, const char* argv[])
 				ave_angleY += angleY[i];
 				ave_angleZ += angleZ[i];
 
-				//if (marker_ids[i] == 18)  //id=18のマーカーに
-				//{
 					cv::Mat tmatrix = (cv::Mat_<double>(3, 1) << tvecs[i][0], tvecs[i][1], tvecs[i][2]);
 					cv::Mat distance = (cv::Mat_<double>(3, 1));
 					cv::Mat screen = (cv::Mat_<double>(3, 1) << marker_center[i][0], marker_center[i][1], 1);
 
-					//distance = rmatrix.inv() * (cameraMatrix.inv() * screen - tmatrix);
-
 					distanceX += tmatrix.at<double>(0, 0) * kx * marker_size;
 					distanceY += tmatrix.at<double>(1, 0) * ky * marker_size;
 					distanceZ += tmatrix.at<double>(2, 0) * kz * marker_size;
-				//}
-
 			}
 
 
@@ -271,41 +243,6 @@ int main(int argc, const char* argv[])
 			ave_angleZ /= marker_ids.size();
 
 		}
-
-
-        /*
-		float angle = 0;  //平均の角度
-		float t[6] = { 0, 0, 0, 0, 0, 0 };
-		if (marker_ids.size() > 0) {
-			for (int i = 0; i < marker_ids.size(); i++)
-			{
-				if (marker_center[i][0] > marker_location[i][2]) {
-
-					if (marker_center[i][1] < marker_location[i][3]) {
-						t[i] = -180 - atan((marker_center[i][1] - marker_location[i][3]) / (marker_center[i][0] - marker_location[i][2])) / (3.14 * 2) * 360;
-					}
-					else {
-						t[i] = 180 - atan((marker_center[i][1] - marker_location[i][3]) / (marker_center[i][0] - marker_location[i][2])) / (3.14 * 2) * 360;
-					}
-				}
-				else {
-					t[i] = atan((marker_center[i][1] - marker_location[i][3]) / (marker_location[i][2] - marker_center[i][0])) / (3.14 * 2) * 360;
-				}
-				if (t[i] > -135) {
-					t[i] = t[i] - 45;
-				}
-				else
-					t[i] = t[i] + 135 + 180;
-
-				t[i] = -t[i];
-
-				angle += t[i];
-			}
-
-
-			angle /= marker_ids.size();  //角度の平均を出す
-		}
-        */
 		 
 	    //サーボモーターを動かす角度を調べる
 		
@@ -318,12 +255,6 @@ int main(int argc, const char* argv[])
 				}else {
 					Servo_angle += atan((360 - average_center[1]) / (average_center[0] - 640)) / (3.14 * 2) * 360;
 				}
-
-                //Servo_angle += atan ( ((360 - average_center[1]) / (average_center[0] - 640)) / (3.14 * 2) * 360 );
-                //Servo_angle += atan((360 - average_center[1]) / (average_center[0] - 640)) / (3.14 * 2) * 360;
-				//Servo_angle = average_center[1];//atan((360 - average_center[1]) / (average_center[0] - 640)) / (3.14 * 2) * 360;
-
-				//Servo_angle++;
 				
 				if(Servo_angle >= 180){
 					Servo_angle -= 180;
@@ -331,14 +262,7 @@ int main(int argc, const char* argv[])
 					Servo_angle += 180;
 				}
 				
-                //Servo_angle = 90;
-
-				//Servo_angleの角度にサーボを動かす
 				sprintf(Servo_angle_char, "%.0f", Servo_angle); 
-				  //Servo_angle_char[3] = '\n';
-				//Serial.write(&Servo_angle_char, 4);
-				  //Serial.write(&finish, 1);
-
 			}
 		}
 		count_frame++;
@@ -350,35 +274,8 @@ int main(int argc, const char* argv[])
 		//真ん中に円を描画
 		circle(image, Point(average_center[0], average_center[1]), 10, Scalar(255, 0, 0), 3, 4);
 
-		/*
-		if (marker_ids.size() > 0)
-		{
-			for (int i = 0; i < marker_ids.size(); i++)
-			{
-				float valuex = marker_corners[i][0].x;
-				char valuex_c[256]; //次の行で使う一時的な変数
-				sprintf(valuex_c, "value=%f", valuex); //変数の値も含めた表示したい文字列をchar型変数に格納
+		flip(image, mirror_image, 1);
 
-				float valuey = marker_corners[i][0].y;
-				char valuey_c[256]; //次の行で使う一時的な変数
-				sprintf(valuey_c, "value=%f", valuey); //変数の値も含めた表示したい文字列をchar型変数に格納
-
-				circle(image, Point(valuex, valuey), 10, Scalar(255, 0, 0), 3, 4);
-			}
-			
-
-			//検出したマーカーの左右反転画像の描画
-			flip(image, mirror_image, 1);
-
-		    //cv::line(image, cv::Point(100, 200), cv::Point(400, 205), cv::Scalar(0, 200, 0), 5, 8);
-			//putText(mirror_image, valuex_c, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
-		}
-		else {*/
-			//検出したマーカーの左右反転画像の描画
-			flip(image, mirror_image, 1);
-		//}
-
-    	//putText(/*mirror_*/image, /*valuex_i*/Servo_angle_char, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
     	putText(/*mirror_*/image, /*valuex_i*/Servo_angle_char, Point(50, 50), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
 
 		//取得した値の表示
@@ -386,27 +283,23 @@ int main(int argc, const char* argv[])
 		cv::imshow("out(Mirror)", /*mirror_*/image);  
 
 		cv::namedWindow("angle", cv::WINDOW_AUTOSIZE);
+
 		char value_x[100];
 		char value_y[100];
 		char value_z[100];
 		sprintf(value_x, "angle X =%f", ave_angleX);
 		sprintf(value_y, "angle Y =%f", ave_angleY);
 		sprintf(value_z, "angle Z =%f", ave_angleZ);
-		// putText(backimage, value_x, Point(50, 100), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
-		// putText(backimage, value_y, Point(50, 150), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
-		// putText(backimage, value_z, Point(50, 200), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
 		putText(backimage, value_x, Point(50, 100), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
 		putText(backimage, value_y, Point(50, 150), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
 		putText(backimage, value_z, Point(50, 200), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
+
 		char value2_x[100];
 		char value2_y[100];
 		char value2_z[100];
 		sprintf(value2_x, "distance X =%f cm", distanceX);
 		sprintf(value2_y, "distance Y =%f cm", distanceY);
 		sprintf(value2_z, "distance Z =%f cm", distanceZ);
-		// putText(backimage, value2_x, Point(50, 250), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
-		// putText(backimage, value2_y, Point(50, 300), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
-		// putText(backimage, value2_z, Point(50, 350), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2, CV_AA);
 		putText(backimage, value2_x, Point(50, 250), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
 		putText(backimage, value2_y, Point(50, 300), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
 		putText(backimage, value2_z, Point(50, 350), FONT_HERSHEY_SIMPLEX, 1.2, Scalar(100, 200, 100), 2);
@@ -423,14 +316,6 @@ int main(int argc, const char* argv[])
 
         //操縦パソコンに送る値のケイサン
 		//地上機体との相対的な位置を計算する
-		/*
-		drone_angle = (atan(distanceY / distanceX) / (3.14 * 2) * 360) + Servo_angle;
-        distanceR = (distanceX / cos(atan(distanceY / distanceX)));
-
-		drone_distanceX = distanceR * cos(drone_angle);
-		drone_distanceY = distanceR * sin(drone_angle);
-		drone_distanceZ = distanceZ;
-		*/
 	    //サーボモーターを動かさない場合
 		drone_distanceX = distanceX;
 		drone_distanceY = distanceY;
@@ -441,14 +326,6 @@ int main(int argc, const char* argv[])
 		drone_angleZ = ave_angleZ;
 
 		//操縦パソコンに送る値
-		/*　
-		drone_distanceX
-		drone_distanceY
-		drone_distanceZ
-        drone_angleX
-		drone_angleY
-		drone_angleZ
-		*/
 		send_data.drone_distanceX = drone_distanceX;
 		send_data.drone_distanceY = drone_distanceY;
 		send_data.drone_distanceZ = drone_distanceZ;
@@ -520,8 +397,6 @@ int CalibrationCamera(VideoCapture& cap, cv::Mat& cameraMat, cv::Mat& distCoeff)
 
 	IM_SIZE.width = (int)cap.get(cv::CAP_PROP_FRAME_WIDTH);
 	IM_SIZE.height = (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-	//Size IM_SIZE = Size(IM_SIZE.width, IM_SIZE.height);
 
 	Mat cameraMatrix;
 	Mat distCoeffs;
@@ -608,27 +483,6 @@ int CalibrationCamera(VideoCapture& cap, cv::Mat& cameraMat, cv::Mat& distCoeff)
 	fs << "distCoeffs" << distCoeffs;
 	fs.release();
 
-	//ーーーrvecsとtvecsの表示（参考）ーーー
-	/*
-	cout << "rotation vector" << endl;
-	for (int i = 0; i < 1; i++)
-	{
-		cout << "cboard" + to_string(i);
-		for (int j = 0; j < 3; j++)
-			cout << "  " << setw(9) << setprecision(4) << rvecs[i].at<double>(j, 0);
-		cout << endl;
-	}
-	cout << endl;
-	cout << "translation vector" << endl;
-	for (int i = 0; i < 1; i++)
-	{
-		cout << "cboard" << to_string(i);
-		for (int j = 0; j < 3; j++)
-			cout << "  " << setw(9) << setprecision(4) << tvecs[i].at<double>(j, 0);
-		cout << endl;
-	}
-	cout << endl << endl;*/
-
 	//ーーー画像を補正するーーー
 	undistort(src_image, dst_image, cameraMatrix, distCoeffs);
 	namedWindow("補正画像");
@@ -643,14 +497,3 @@ int CalibrationCamera(VideoCapture& cap, cv::Mat& cameraMat, cv::Mat& distCoeff)
 
 	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
